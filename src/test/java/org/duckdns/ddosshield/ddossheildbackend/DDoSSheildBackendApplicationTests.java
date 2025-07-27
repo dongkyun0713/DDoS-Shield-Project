7f76335fed7a125b7f53ed9f1d0c8a78828d4bb3
dongkyunKim <kyun010713@gmail.com>
2025-07-27 17:44:21 +0900

    chore(#1): 프로젝트 생성

---
+++ src/test/java/org/duckdns/ddosshield/ddossheildbackend/DDoSSheildBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.duckdns.ddosshield.ddossheildbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DDoSSheildBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
